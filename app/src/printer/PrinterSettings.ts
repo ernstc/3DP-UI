2ea97a81b8c44f08811ea8fc5772f2b32cd02be5
Ernesto Cianciotta <ernesto.cianciotta@gmail.com>
2021-05-05 12:50:24 +0200

    Added comments

--- app/src/printer/PrinterSettings.ts
+++ app/src/printer/PrinterSettings.ts
@@ -25,16 +25,59 @@ export abstract class PrinterSettings {
 
 
 export enum PrinterStatusEnum {
+    /// <summary>
+    /// The firmware is being updated
+    /// </summary>
     updating,
+    
+    /// <summary>
+    /// The machine is turned off (i.e. the input voltage is too low for operation)
+    /// </summary>
     off,
+    
+    /// <summary>
+    /// The machine has encountered an emergency stop and is ready to reset
+    /// </summary>
     halted,
+    
+    /// <summary>
+    /// The machine is about to pause a file job
+    /// </summary>
     pausing,
+    
+    /// <summary>
+    /// The machine has paused a file job
+    /// </summary>
     paused,
+    
+    /// <summary>
+    /// The machine is about to resume a paused file job
+    /// </summary>
     resuming,
+    
+    /// <summary>
+    /// The machine is processing a file job
+    /// </summary>
     processing,
+    
+    /// <summary>
+    /// The machine is simulating a file job to determine its processing time
+    /// </summary>
     simulating,
+    
+    /// <summary>
+    /// The machine is busy doing something (e.g. moving)
+    /// </summary>
     busy,
+    
+    /// <summary>
+    /// The machine is changing the current tool
+    /// </summary>
     changingTool,
+    
+    /// <summary>
+    /// The machine is on but has nothing to do
+    /// </summary>
     idle
 }
 
